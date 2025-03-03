55928e392aad728a14f4bc82b897bcf3bd5fafcd
LingJiang9 <D22128264@mytudublin.ie>
2025-03-03 20:19:40 +0000

    emscriptinstallation

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <emscripten.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -6,7 +7,7 @@
 
 #define LIST_SIZE 16
 
-void print_128bit_block(unsigned char *block) {
+void print_128bit_block(unsigned char* block) {
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
       unsigned char value = BLOCK_ACCESS(block, i, j);
@@ -26,8 +27,8 @@ int main() {
   unsigned char key[16] = {43,  126, 21, 22,  40, 174, 210, 166,
                            171, 247, 21, 136, 9,  207, 79,  60};
 
-  unsigned char *ciphertext = aes_encrypt_block(plaintext, key);
-  unsigned char *recovered_plaintext = aes_decrypt_block(ciphertext, key);
+  unsigned char* ciphertext = aes_encrypt_block(plaintext, key);
+  unsigned char* recovered_plaintext = aes_decrypt_block(ciphertext, key);
 
   printf("############ ORIGINAL PLAINTEXT ###########\n");
   print_128bit_block(plaintext);
@@ -43,3 +44,15 @@ int main() {
 
   return 0;
 }
+
+#ifdef __EMSCRIPTEN__
+EMSCRIPTEN_KEEPALIVE
+unsigned char* encrypt_block_js(unsigned char* plaintext, unsigned char* key) {
+  return aes_encrypt_block(plaintext, key);
+}
+
+EMSCRIPTEN_KEEPALIVE
+unsigned char* decrypt_block_js(unsigned char* ciphertext, unsigned char* key) {
+  return aes_decrypt_block(ciphertext, key);
+}
+#endif
